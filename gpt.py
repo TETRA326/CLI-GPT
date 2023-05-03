e3dd0f3d9f85dfdfc8ef360f84749f3bedd4143e
jimmy <91847844+yeeter727@users.noreply.github.com>
2023-05-03 15:01:35 +0000

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -47,7 +47,7 @@ print(Fore.BLUE + "Commands:")
 print(Style.BRIGHT + "exit" + Style.NORMAL + "  [quit, x, q]    - quit conversation")
 print(Style.BRIGHT + "clear" + Style.NORMAL + "                 - clear the screen")
 print(Style.BRIGHT + "log" + Style.NORMAL + "   [history]       - shows the previous messages in the conversation"  + Style.RESET_ALL)
-print(Fore.CYAN + "Hint: You can copy using 'CTRL + SHIFT + C' and paste using 'CTRL + SHIFT + V'" + Style.RESET_ALL)
+print(Fore.CYAN + "Tip: You can copy using 'CTRL + SHIFT + C' and paste using 'CTRL + SHIFT + V'" + Style.RESET_ALL)
 
 # Set up OpenAI API key
 openai.api_key = ""
